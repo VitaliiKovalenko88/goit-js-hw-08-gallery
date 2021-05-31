3863dafd4fe0b626ff35cc5fd5e10727bdd1f100
VitaliiKovalenko88 <vintilyator88@gmail.com>
2021-05-31 12:35:42 +0200

    cделал полностью тз ДЗ-8.

--- js/gallery.js
+++ js/gallery.js
@@ -1,16 +1,12 @@
 import galleryItems from './gallery-items.js';
 
-// 4.Подмена значения атрибута src элемента img.lightbox**image.
-// 5.Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
-// 6.Очистка значения атрибута src элемента img.lightbox**image.
-
 // Создание и рендер разметки по массиву данных и предоставленному шаблону.
 const refs = {
   galleryList: document.querySelector('.js-gallery'),
   overlay: document.querySelector('.lightbox__overlay'),
   mdl: document.querySelector('.js-lightbox'),
   imageMdl: document.querySelector('.lightbox__image'),
-  closeBtn: document.querySelector('.lightbox__button'),
+  closeBtn: document.querySelector('[data-action="close-lightbox"]'),
 };
 
 const createGalleryItemsMarcup = gallery =>
@@ -35,32 +31,63 @@ const createGalleryItemsMarcup = gallery =>
 
 // Открытие модального окна по клику на элементе галереи.
 const onOpenModal = event => {
+  event.preventDefault();
+
   const imageTarget = event.target;
 
   if (!imageTarget.classList.contains('gallery__image')) {
     return;
   }
-  event.preventDefault();
 
   refs.mdl.classList.add('is-open');
   refs.imageMdl.src = imageTarget.dataset.source;
   refs.imageMdl.alt = imageTarget.alt;
-
   window.addEventListener('keydown', onArrowPress);
   window.addEventListener('keydown', onEscPress);
 };
 
+// Реализация делегирования на галерее ul.js-gallery и получение url большого
+// изображения.
+
+const onArrowPress = event => {
+  if (event.code !== 'ArrowLeft' && event.code !== 'ArrowRight') {
+    return;
+  }
+
+  const currentImage = galleryItems.find(
+    img => image.original === refs.imageMdl.src,
+  );
+
+  let index =
+    event.code === 'ArrowLeft'
+      ? galleryItems.indexOf(currentImage) - 1
+      : galleryItems.indexOf(currentImage) + 1;
+  if (index < 0) {
+    index = galleryItems.length - 1;
+  }
+  if (index > galleryItems.length - 1) {
+    index = 0;
+  }
+
+  // Подмена значения атрибута src элемента img.lightbox**image.
+  refs.imageMdl.src = galleryItems[index].original;
+  refs.imageMdl.alt = galleryItems[index].description;
+};
+
+// Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
 const onCloseModal = () => {
   window.removeEventListener('keydown', onEscPress);
   window.removeEventListener('keydown', onArrowPress);
+
   refs.mdl.classList.remove('is-open');
+  // Очистка значения атрибута src элемента img.lightbox**image.
   refs.imageMdl.src = '';
   refs.imageMdl.alt = '';
 };
 
 const onOverlayClick = event => {
   if (event.target === refs.overlay) {
-    onCloseModal;
+    onCloseModal();
   }
 };
 
@@ -69,32 +96,8 @@ const onEscPress = event => {
     onCloseModal();
   }
 };
-// Реализация делегирования на галерее ul.js-gallery и получение url большого
-// изображения.
-
-const onArrowPress = event => {
-  if (event.code !== 'Arrowleft' && event.code !== 'ArrowRight') {
-    return;
-  }
-
-  const currentImage = galleryItems.find(
-    img => img.original === refs.imageMdl.src,
-  );
-
-  const galleryIndex = galleryItems.indexOf(currentImage);
-  let index;
-  index = event.code === 'ArrowLeft' ? galleryIndex - 1 : galleryIndex + 1;
-  if (index < 0) {
-    index = galleryItems.length - 1;
-  }
-  if (index > galleryItems.length - 1) {
-    index = 0;
-  }
-  refs.imageMdl.src = galleryItems[index].original;
-  refs.imageMdl.alt = galleryItems[index].description;
-};
 
 refs.galleryList.innerHTML = createGalleryItemsMarcup(galleryItems);
 refs.galleryList.addEventListener('click', onOpenModal);
-refs.galleryList.addEventListener('click', onCloseModal);
-refs.galleryList.addEventListener('click', onOverlayClick);
+refs.closeBtn.addEventListener('click', onCloseModal);
+refs.overlay.addEventListener('click', onOverlayClick);
